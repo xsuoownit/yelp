3764b3082b9f51487f4dc3f9eb1da6c088742b2f
Xin Suo <xsuo@groupon.com>
2015-11-02 23:56:20 -0800

    Initial Commit

---
+++ yelp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  yelp
+//
+//  Created by Xin Suo on 11/2/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ yelp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  yelp
+//
+//  Created by Xin Suo on 11/2/15.
+//  Copyright © 2015 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
